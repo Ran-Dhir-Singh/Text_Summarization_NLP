504ee3274ceb91ca4ea4c0e479c8e9f2923997aa
unknown <randhirsingh7777777@gmail.com>
2023-09-06 13:40:26 +0530

    Data Transformation Update

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeling
-from src.logging import logger
 from src.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
-
+from src.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -24,6 +23,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
 
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
